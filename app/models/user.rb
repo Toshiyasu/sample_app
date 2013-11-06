23b1a75e3234b0d7da8cadabafb40ea627774f6c
Yasutoshi <hal_c06_49@yahoo.co.jp>
2013-11-06 12:02:36 +0900

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # email属性を小文字に変換してメールアドレスの一意性を保証する
   before_save { self.email = email.downcase }
   
+  before_create :create_remember_token
+  
   validates :name, presence: true, length: { maximum: 50 }
   
   # 正規表現の定数
@@ -14,4 +16,18 @@ class User < ActiveRecord::Base
   has_secure_password
   
   validates :password, length: { minimum: 6 }
+  
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
